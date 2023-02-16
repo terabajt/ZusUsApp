8a86b33310b561aa1e076422f76d8cbc56bda9eb
Michal Pasynek laptop <michalpasynek@gmail.com>
2023-02-16 23:41:47 +0100

    ..

--- src/app/companies/companies-routing.module.ts
+++ src/app/companies/companies-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CompaniesSummaryComponent } from './companies-summary/companies-summary.component';
 import { EditCompanyItemComponent } from './edit-company-item/edit-company-item.component';
+import { PdfCreateComponent } from './pdf-create/pdf-create.component';
 
 const routes: Routes = [
   { path: '', component: CompaniesSummaryComponent },
+  { path: 'print', component: PdfCreateComponent },
   { path: ':key', component: EditCompanyItemComponent }
 ];
 
--- src/app/companies/pdf-create/pdf-create.component.ts
+++ src/app/companies/pdf-create/pdf-create.component.ts
@@ -1,16 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import jsPDF from 'jspdf';
 import html2canvas from 'html2canvas';
+import { CompaniesService } from 'src/app/core/services/companies.service';
+import { combineLatest, find, map, Observable, tap } from 'rxjs';
+import { Company_item } from 'src/app/models/company-item';
+import { Company } from 'src/app/models/company';
+import { ActivatedRoute, Route } from '@angular/router';
 
 @Component({
   selector: 'app-pdf-create',
   templateUrl: './pdf-create.component.html',
   styleUrls: ['./pdf-create.component.scss']
 })
-export class PdfCreateComponent implements OnInit {
-  constructor() {}
+export class PdfCreateComponent {
+  constructor(private comapniesService: CompaniesService, private route: ActivatedRoute) {}
+  @Input() company_item: Company_item;
+  @Input() company: Company;
 
-  ngOnInit(): void {}
+  //Get data to show on print page
+  companyItems$: Observable<Company_item[]> = this.comapniesService.getItemsInfos();
+  companiesData$: Observable<Company[]> = this.comapniesService.getCompaniesInfos();
+
+  // NEW SOLUTION
+  items$ = combineLatest({
+    companyItems: this.companyItems$,
+    companiesData: this.companiesData$
+  }).pipe(
+    map(({ companyItems, companiesData }) => {
+      return companyItems.map(companyItem => ({
+        companyItem: companyItem,
+        companyData: companiesData.find(companyData => companyData.company_id === companyItem.company_id)
+      }));
+    })
+  );
+
+  items2$: any = this.items$;
+
+  getItemToPrint;
+
+  getItem(key: string) {
+    this.getItemToPrint = this.items$.subscribe(res => res.find(res => key === res.companyItem.key));
+  }
+
+  ngAfterViewInit() {
+    const key = this.route.snapshot.params['key'];
+    this.getItem('-NO6C9HwpQZlXnzrB14o');
+  }
 
   //Print method
   onConfirm() {
